5e7ac07bc55f94eed26aacd306df78d1763c550a
Unknown <limper545@live.com>
2018-02-15 19:44:37 +0100

    Anpassung Projektstruktur

---
+++ Core/DBGUIHandler.cs
@@ -0,0 +1,30 @@
+﻿using Bab_1.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bab_1.GUI
+{
+    class DBGUIHandler
+    {
+        public string GetConnectionStatus()
+        {
+            if (DBHandler.CreateDatabaseConnection())
+            {
+                return "Connected to Database";
+            }
+            else
+            {
+                return "Connection to Database failed Try to reconecting...";
+            }
+        }
+
+        public bool LoginDataCorrect(String username, String password)
+        {
+            DBHandler.CheckLogin(username, password);
+            return true;
+        }
+    }
+}
---
+++ GUI/StartMenue.cs
@@ -0,0 +1,46 @@
+﻿using Bab_1.GUI;
+using BaB_1._0.GUI;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace BaB_1
+{
+    public partial class StartMenu : Form
+    {
+        DBGUIHandler handler = new DBGUIHandler();
+        public StartMenu()
+        {
+            InitializeComponent();
+            dbConnection.Text = handler.GetConnectionStatus();
+            dbConnection.BackColor = Color.Transparent;
+
+            gameTitleLabel.Text = "BaB";
+            gameTitleLabel.BackColor = Color.Transparent;
+        }
+
+        private void Login_Btn_Click(object sender, EventArgs e)
+        {
+            handler.LoginDataCorrect("a", "b");
+        }
+
+        private void close_Btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Login_Btn_Click_1(object sender, EventArgs e)
+        {
+            loginDataForm loginForm = new loginDataForm();
+            loginForm.StartPosition = FormStartPosition.CenterParent;
+            loginForm.Show(this);
+        }
+
+        private void reg_Btn_Click(object sender, EventArgs e)
+        {
+            registrationForm regi = new registrationForm();
+            regi.Show();
+        }
+    }
+}
